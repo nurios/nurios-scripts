007972be89a3d14fc8fc310ecc8eea3542f7a61d
Oytun <oytunozdemir@gmail.com>
2014-10-10 00:38:38 +0300

    Güncelleştirme

--- webapi/app/config.php
+++ webapi/app/config.php
@@ -1,7 +1,7 @@
 <?php
 /* Some def staff */
 
-define('APPNAME','Core Framework Example Daemon');
+define('APPNAME','NuriOS Background WebAPI');
 define('APPVER','0.1');
 define('RUNTIME_IP','0.0.0.0'); // parametreden alınabilir
 define('RUNTIME_PORT','8080'); // parametreden alınabilir
@@ -13,13 +13,4 @@ define('CONTROLLER_FOLDER','app/controllers/');
 define('CORE_FOLDER','core/');
 define('PUBLIC_FOLDER','public/');
 define('HOME_CONTROLLER','home');
-
-/* Database staff */
-
-define('DB_DRIVER','mysql');
-define('DB_SERVER','localhost');
-define('DB_DATABASE','exampleddb');
-define('DB_USERNAME','exampleuser');
-define('DB_PASSWORD','exampledb');
-define('DB_PORT','3306');
 ?>
